552e2f6f3fddc119d3c3d25f5c2626c79a6afd88
m.amin.rayej <m.amin.rayej@gmail.com>
2020-12-31 14:15:41 +0330

    implement slicing for nd array

--- src/ndarray/array/mod.rs
+++ src/ndarray/array/mod.rs
@@ -1,4 +1,9 @@
-use std::{borrow::Cow, ops::Index};
+use std::{
+    borrow::Cow,
+    ops::{Bound, Index, Range, RangeBounds},
+};
+
+use Bound::{Excluded, Included, Unbounded};
 
 #[derive(Debug, Clone, Copy)]
 struct IdxMap {
@@ -13,6 +18,10 @@ impl IdxMap {
 
     fn map(&self, idx: usize) -> usize {
         (self.m * (idx as isize) + self.b) as usize
+    }
+
+    fn append_b(&mut self, b: isize) {
+        self.b += self.m * b;
     }
 }
 
@@ -69,7 +78,7 @@ impl<'a, T: Clone, const D: usize> Array<'a, T, D> {
             vec: Cow::from(&*self.vec),
             shape,
             strides,
-            idx_maps
+            idx_maps,
         }
     }
 
@@ -105,19 +114,14 @@ impl<'a, T: Clone, const D: usize> Array<'a, T, D> {
 
         let idx_map = &mut idx_maps[axis];
 
-        if idx_map.m == -1 {
-            idx_map.m = 1;
-            idx_map.b = idx_map.b - (self.shape[axis] - 1) as isize
-        } else {
-            idx_map.m = -1;
-            idx_map.b = idx_map.b + (self.shape[axis] - 1) as isize
-        }
+        idx_map.append_b((self.shape[axis] - 1) as isize);
+        idx_map.m *= -1;
 
         Array {
             vec: Cow::from(&*self.vec),
             shape: self.shape.clone(),
             strides: self.strides.clone(),
-            idx_maps
+            idx_maps,
         }
     }
 
@@ -138,16 +142,47 @@ impl<'a, T: Clone, const D: usize> Array<'a, T, D> {
             vec: Cow::from(&*self.vec),
             shape,
             strides,
-            idx_maps
+            idx_maps,
         }
     }
 
     pub fn reshape<const S: usize>(&self, shape: [usize; S]) -> Array<'a, T, S> {
-        // TODO: Check if c-contigous
-
         let vec = self.iter().cloned().collect();
 
         Array::init(vec, shape)
+    }
+
+    pub fn slice<R: RangeBounds<usize>>(&'a self, ranges: [R; D]) -> Array<'a, T, D> {
+        let mut shape = self.shape.clone();
+        let strides = self.strides.clone();
+        let mut idx_maps = self.idx_maps.clone();
+
+        let bounded_ranges = ranges
+            .iter()
+            .enumerate()
+            .map(|(axis, range)| bounded_range_of(self.shape[axis], range))
+            .collect::<Vec<Range<usize>>>();
+
+        bounded_ranges.iter().enumerate().for_each(|(axis, range)| {
+            if range.end > self.shape[axis] {
+                panic!(
+                    "Range: [{},{}) is out of bounds for axis: {}",
+                    range.start, range.end, axis
+                )
+            }
+        });
+
+        for axis in 0..D {
+            idx_maps[axis].append_b((bounded_ranges[axis].start) as isize);
+            shape[axis] = bounded_ranges[axis].end - bounded_ranges[axis].start;
+        }
+
+        Array {
+            vec: Cow::from(&*self.vec),
+            shape,
+            strides,
+            idx_maps,
+        }
     }
 }
 
@@ -213,6 +248,20 @@ impl<'a, T: Clone, const D: usize> Iterator for Iter<'a, T, D> {
         self.increment_indices();
 
         item
+    }
+}
+
+fn bounded_range_of<R: RangeBounds<usize>>(upper_bound: usize, range: &R) -> Range<usize> {
+    match (range.start_bound(), range.end_bound()) {
+        (Included(&start), Included(&end)) => start..end.checked_add(1).unwrap_or(upper_bound),
+        (Included(&start), Excluded(&end)) => start..end,
+        (Included(&start), Unbounded) => start..upper_bound,
+        (Excluded(&start), Included(&end)) => start + 1..end.checked_add(1).unwrap_or(upper_bound),
+        (Excluded(&start), Excluded(&end)) => start + 1..end,
+        (Excluded(&start), Unbounded) => start + 1..upper_bound,
+        (Unbounded, Included(&end)) => 0..end + 1,
+        (Unbounded, Excluded(&end)) => 0..end,
+        (Unbounded, Unbounded) => 0..upper_bound,
     }
 }
 
@@ -328,15 +377,52 @@ mod tests {
         // 1 2 3
         let array = array.flip(0);
 
-        assert_eq!(array.iter().copied().collect::<Vec<usize>>(), vec![4, 5, 6, 1, 2, 3]);
+        assert_eq!(
+            array.iter().copied().collect::<Vec<usize>>(),
+            vec![4, 5, 6, 1, 2, 3]
+        );
     }
 
     #[test]
-    fn d1() {
-        let array = Array::init(vec![1, 2, 3, 4, 5, 6], [2, 3]);
+    fn bounded_range() {
+        assert_eq!(bounded_range_of(10, &(..3)), 0..3);
+        assert_eq!(bounded_range_of(10, &(..)), 0..10);
+        assert_eq!(bounded_range_of(10, &(1..)), 1..10);
+        assert_eq!(bounded_range_of(10, &(1..3)), 1..3);
+        assert_eq!(bounded_range_of(10, &(1..=3)), 1..4);
+    }
 
-        let array = array.reshape([6]);
+    #[test]
+    fn slicing() {
+        // 2-D array:
+        // 1   2  3  4
+        // 5   6  7  8
+        // 9  10 11 12
+        // 13 14 15 16
+        let array = Array::init(
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            [4, 4],
+        );
 
-        assert_eq!(array[[0]], 1)
+        // flip the array across axis=0
+        // 13 14 15 16
+        // 9  10 11 12
+        // 5  6  7  8
+        // 1  2  3  4
+        let flipped = array.flip(0);
+
+        // slice the center of the array
+        // 10 11
+        // 6  7
+        let slice = flipped.slice([1..3, 1..3]);
+
+        // 11 10
+        // 7  6
+        let flip_of_slice = slice.flip(1);
+
+        assert_eq!(
+            flip_of_slice.iter().copied().collect::<Vec<usize>>(),
+            vec![11, 10, 7, 6]
+        );
     }
 }
