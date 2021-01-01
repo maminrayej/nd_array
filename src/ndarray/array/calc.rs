8a3131228e537490a8e12ee2fa21df89f8967ebe
m.amin.rayej <m.amin.rayej@gmail.com>
2021-01-01 11:04:49 +0330

    implement max,arg_max,min,arg_min

--- src/ndarray/array/calc.rs
+++ src/ndarray/array/calc.rs
@@ -1,7 +1,59 @@
 use crate::Array;
 
-impl<'a, T: Clone + PartialOrd, const D: usize> Array<'a, T, D> {
-    pub fn max(&self, axis: usize) -> &T {
-        todo!()
+impl<'a, T: Clone + Ord, const D: usize> Array<'a, T, D> {
+    pub fn max(&self) -> Option<T> {
+        self.flat().max().cloned()
+    }
+
+    pub fn arg_max(&self) -> Vec<usize> {
+        let mut positions = vec![];
+
+        if let Some(max) = self.max() {
+            for (index, value) in self.flat().enumerate() {
+                if value == &max {
+                    positions.push(index)
+                }
+            }
+        }
+
+        positions
+    }
+
+    pub fn max_across(&self, axis: usize) -> Vec<Option<T>> {
+        self.axis_view(axis).map(|view| view.max()).collect()
+    }
+
+    pub fn arg_max_across(&self, axis: usize) -> Vec<Option<usize>> {
+        self.axis_view(axis)
+            .map(|view| view.arg_max().get(0).copied())
+            .collect()
+    }
+
+    pub fn min(&self) -> Option<T> {
+        self.flat().min().cloned()
+    }
+
+    pub fn arg_min(&self) -> Vec<usize> {
+        let mut positions = vec![];
+
+        if let Some(min) = self.min() {
+            for (index, value) in self.flat().enumerate() {
+                if value == &min {
+                    positions.push(index)
+                }
+            }
+        }
+
+        positions
+    }
+
+    pub fn min_across(&self, axis: usize) -> Vec<Option<T>> {
+        self.axis_view(axis).map(|view| view.min()).collect()
+    }
+
+    pub fn arg_min_across(&self, axis: usize) -> Vec<Option<usize>> {
+        self.axis_view(axis)
+            .map(|view| view.arg_min().get(0).copied())
+            .collect()
     }
 }
